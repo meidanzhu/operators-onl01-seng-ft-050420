9c8b91dba0ba0ef68b3a1eec98ffff1e909ca18e
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-05 17:39:04 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,17 @@
 def unsafe?(speed)
-
+ #It should return true if the speed is either below 40 or above 60
+ 	if speed > 60
+	 return true
+ elsif speed < 40
+	 return true
+ 	else
+	 return false
+	end
 end
 
 
 
 def not_safe?(speed)
-	
+	(speed < 40 || speed > 60) ? true : false
+
 end
-	
-
-
