9048a9c9fdb22449d462c025d0b2a36949f861b7
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-05-05 17:29:08 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,7 +1,8 @@
 def unsafe?(speed)
  #It should return true if the speed is either below 40 or above 60
- 	if speed < 40 || speed > 60
+ 	if speed < 40
 	 "true"
+ 	elsif speed > 60
  	else
 	 "false"
 	end
